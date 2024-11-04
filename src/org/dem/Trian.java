e34cf330bd46b9c6a1afb2518b381bd71a14f754
Devaaaga <devaagar914@gmail.com>
2024-11-04 15:08:13 +0530

    done

---
+++ src/org/dem/Trian.java
@@ -0,0 +1,30 @@
+package org.dem;
+
+import org.poly.TrainDetails;
+
+public class Trian {
+
+public void trainDetails() {
+	System.out.println("train details:");
+}
+public void trainDetails(int trainspeed) {
+	System.out.println("trainspeed:"+trainspeed);
+}
+public void trainDetails(int trainnumber,String name) {
+	System.out.println("trainnumber:"+trainnumber);
+	System.out.println("trainname:"+name);
+}
+public void trainDetails(String cityname,int ticketprice) {
+	System.out.println("train cityname:"+cityname);
+	System.out.println("train ticketprice"+ticketprice);
+}
+public static void main(String[] args) {
+	TrainDetails t = new TrainDetails();
+	t.trainDetails();
+	t.trainDetails(600);
+	t.trainDetails(12606,"pallavan");
+	t.trainDetails("Trichy", 173);
+}
+}
+	
+	
